958c5e7b3f55a046e5ab6b7f8dc14207a77568e2
GGG <kuranberitorappu@gmail.com>
2022-06-01 06:15:20 +0000

    Fix: [blogルートを複数形に修正]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :blog do
+  # エラー原因：resources :blog doのblogが単数形になっていたので
+  # 修正の意図：noruteerrorを解除する為
+  resources :blogs do
     resources :comments, only: [:create, :destroy]
   end
 end
