592c616cf7f5367c39c46d7d620c870237a54880
GGG <kuranberitorappu@gmail.com>
2022-06-01 06:33:25 +0000

    Fix: [has_many :commentsが単数形になっていたため修正]

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,7 @@
 class Blog < ApplicationRecord
-  has_many :comment
+  # エラー原因: has_many :commentが単数形になっていた
+  # 修正の意図: 参照する際に外部キー名でerrorが出ていたため修正
+  has_many :comments
 
   validates :title, presence: true
   validates :content, presence: true
